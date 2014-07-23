c0fd935f90b65b07c46de027746d7360b5039a72
Zachary Kazanski <kazanski.zachary@gmail.com>
2014-07-23 10:36:33 -0400

    fixed potential attribute error by navigating to waffled page

--- waffle/middleware.py
+++ waffle/middleware.py
@@ -8,19 +8,22 @@ from waffle.models import Flag
 
 class WaffleMiddleware(object):
     def process_request(self, request):
-        if request.user.is_authenticated():
-            for cookie in request.COOKIES:
-                if Flag.objects.filter(name=cookie.strip('dwf_')).exists():
-                    pk = request.user.pk
-                    flag = Flag.objects.get(name=cookie.strip('dwf_'))
-                    if flag.rollout is True:
-                        if pk not in flag.user_pks and request.COOKIES.get("dwf_" + flag.name) == 'True':
-                            flag.user_pks.append(request.user.pk)
-                            flag.save()
-                    for pk in flag.user_pks:
-                        if not User.objects.filter(pk=pk).exists():
-                            flag.user_pks.remove(pk)
-                            flag.save()
+        try:
+            if request.user.is_authenticated():
+                for cookie in request.COOKIES:
+                    if Flag.objects.filter(name=cookie.strip('dwf_')).exists():
+                        pk = request.user.pk
+                        flag = Flag.objects.get(name=cookie.strip('dwf_'))
+                        if flag.rollout is True:
+                            if pk not in flag.user_pks and request.COOKIES.get("dwf_" + flag.name) == 'True':
+                                flag.user_pks.append(request.user.pk)
+                                flag.save()
+                        for pk in flag.user_pks:
+                            if not User.objects.filter(pk=pk).exists():
+                                flag.user_pks.remove(pk)
+                                flag.save()
+        except AttributeError:
+            pass
 
     def process_response(self, request, response):
         secure = getattr(settings, 'WAFFLE_SECURE', False)
@@ -44,15 +47,18 @@ class WaffleMiddleware(object):
                 response.set_cookie(name, value=value)
 
         # give user cookie true if in database for flag
-        if request.user.is_authenticated():
-            pk = request.user.pk
-            flags = Flag.objects.all()
-            for flag in flags:
-                if pk in flag.user_pks:
-                    try:
-                        response.delete_cookie(str('dwf_' + flag.name))
-                    except:
-                        pass
-                    response.set_cookie(str('dwf_' + flag.name), value=True,
-                                        max_age=2592000, secure=secure)
+        try:
+            if request.user.is_authenticated():
+                pk = request.user.pk
+                flags = Flag.objects.all()
+                for flag in flags:
+                    if pk in flag.user_pks:
+                        try:
+                            response.delete_cookie(str('dwf_' + flag.name))
+                        except:
+                            pass
+                        response.set_cookie(str('dwf_' + flag.name), value=True,
+                                            max_age=2592000, secure=secure)
+        except AttributeError:
+            pass
         return response
